0be16a06df802fced9c7ec6cf2f289738a846e27
Mahima Lalwani <lalwni.mahi@gmail.com>
2026-06-06 17:00:27 +0530

    Add responsive hero section and mobile navigation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,29 @@
 import React from 'react'
 import './Hero.css'
-import image from '../assets/image.png'
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
+import Profile from '../assets/Profile.jpeg'
 
 const Hero = () => {
     return (
         <div className='hero'>
             <div className='Left-Hero'>
                 <p className='intro'>Hi, I am Mahima</p>
-                <h1 className='occupation'> I am a <b>Frontend Developer</b> </h1>
-                <p className="description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti et voluptatem fugiat magnam ullam aperiam perferendis incidunt repudiandae illum cupiditate.</p>
-                <button>Download My Resume</button>
+                <h1 className='occupation'> I am a <span>Frontend Developer</span> </h1>
+                <p className="description">Building responsive and interactive web experiences with React and modern JavaScript.</p>
+                <div className="buttons">
+                    <button>Download Resume</button>
+                    <a href="#contact">
+                        <button id='hire'>Hire Me</button>
+                    </a>
+                </div>
+                <div className="socials">
+                    <a href="https://github.com/codebymahima" target='_blank'><FaGithub /></a>
+                    <a href="https://www.linkedin.com/in/mahima-lalwani/" target='_blank'><FaLinkedin /></a>
+                    <a href="codebymahima@gmail.com" target='_blank'><FaEnvelope /></a>
+                </div>
             </div>
             <div className="ImageSection">
-             
-                <img src={image} alt="Profile Picture" />
-                
+                <img src={Profile} alt="Profile Picture" />
             </div>
         </div>
     )
